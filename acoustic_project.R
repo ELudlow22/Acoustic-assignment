05d107917d86006502cf754971515a8a50f22683
Elliott Ludlow <e.ludlow2@newcastle.ac.uk>
2021-05-07 16:29:21 +0100

    Added data for song thrushes

--- acoustic_project.R
+++ acoustic_project.R
@@ -11,7 +11,7 @@ source("nes8010.R")
 
 
 
-#Specify the type of data you wish to obtain. Here, tawny owl (Strix aluco) song call data is being obtained for both male and female, with length of audio clip being 
+#Specify the type of data you wish to obtain. Here, tawny owl (Strix aluco) song call data is being obtained for both male and female, with length of audio clip being 5-25 seconds
 
 tawny_male <- query_xc(qword = 'Strix aluco type:male len:5-25', download = FALSE)
 tawny_fem  <- query_xc(qword = 'Strix aluco type:female len:5-25', download = FALSE)
@@ -111,6 +111,87 @@ ggplot(tawny_sco, aes(x=PC1, y=PC2, colour=group_code)) +
    geom_point() 
 
 
+
+#Complete the same indices but for song thrushes but comparing song and alarm calls rather than differences between genders
+
+
+#Specify the type of data you wish to obtain. Here, song thrush (Turdus philomelos) song call data is being obtained for both song and alarm calls, with length of audio clip being 5-25 seconds
+songthrush_songs <- query_xc(qword = 'Turdus philomelos  type:song len:5-25', download = FALSE)
+
+songthrush_alarm <- query_xc(qword = 'Turdus philomelos  type:alarm len:5-25', download = FALSE)
+
+
+
+#Map for the song thrush data points
+map_xc(songthrush_songs, leaflet.map = TRUE)
+
+# # Create subfolders in your RStudio Project for song calls and alarm calls
+dir.create(file.path("songthrush_songs"))
+dir.create(file.path("songthrush_alarm"))
+#
+# # Download the .MP3 files into two separate sub-folders
+query_xc(X = songthrush_songs, path="songthrush_songs")
+query_xc(X = songthrush_alarm, path="songthrush_alarm")
+
+
+# Rename song thrush files ----
+# library(stringr) # part of tidyverse
+# 
+old_files <- list.files("songthrush_songs", full.names=TRUE)
+new_files <- NULL
+for(file in 1:length(old_files)){
+   curr_file <- str_split(old_files[file], "-")
+   new_name  <- str_c(c(curr_file[[1]][1:2], "-song_", curr_file[[1]][3]), collapse="")
+   new_files <- c(new_files, new_name)
+}
+file.rename(old_files, new_files)
+
+old_files <- list.files("songthrush_alarm", full.names=TRUE)
+new_files <- NULL
+for(file in 1:length(old_files)){
+   curr_file <- str_split(old_files[file], "-")
+   new_name  <- str_c(c(curr_file[[1]][1:2], "-alarm_", curr_file[[1]][3]), collapse="")
+   new_files <- c(new_files, new_name)
+}
+file.rename(old_files, new_files)
+
+# Put song thrush files into appropriate folders 
+dir.create(file.path("songthrush_audio"))
+file.copy(from=paste0("songthrush_songs/",list.files("songthrush_songs")),
+          to="songthrush_audio")
+file.copy(from=paste0("songthrush_alarm/",list.files("songthrush_alarm")),
+          to="songthrush_audio")
+
+# Convert to .WAV format and delete old mp3
+mp32wav(path="songthrush_audio", dest.path="songthrush_audio")
+unwanted_mp3 <- dir(path="songthrush_audio", pattern="*.mp3")
+file.remove(paste0("songthrush_audio/", unwanted_mp3))
+
+
+songthrush_wav <- readWave("songthrush_audio/Turdusphilomelos-song_243908.wav")
+songthrush_wav
+
+# Oscillogram and spectrogram for song thrush song example
+# Oscillogram
+oscillo(songthrush_wav)
+oscillo(songthrush_wav, from = 0.59, to = 0.60)
+# Spectrogram for song thrush
+SpectrogramSingle(sound.file = "songthrush_audio/Turdusphilomelos-song_243908.wav",
+                  Colors = "Colors")
+
+# Feature extraction for song thrushes via MFCC and PCA ####
+songthrush_mfcc <- MFCCFunction(input.dir = "songthrush_audio",
+                                max.freq=7000)
+dim(songthrush_mfcc)
+songthrush_pca <- ordi_pca(songthrush_mfcc[, -1], scale=TRUE)
+summary(songthrush_pca)$cont[[1]][1:3,1:4]
+
+# Plot PCA scores
+songthrush_sco <- ordi_scores(songthrush_pca, display="sites")
+songthrush_sco <- mutate(songthrush_sco, group_code = songthrush_mfcc$Class)
+
+ggplot(songthrush_sco, aes(x=PC1, y=PC2, colour=group_code)) +
+   geom_point() 
 
 
 
