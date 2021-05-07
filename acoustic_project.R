e62be14cf2ff9470186568a9be5ff7df9b45675a
Elliott Ludlow <e.ludlow2@newcastle.ac.uk>
2021-05-07 16:04:55 +0100

    Solved the problem that was preventing the separation of male and female data

--- acoustic_project.R
+++ acoustic_project.R
@@ -1,77 +1,74 @@
+
+library(ggplot2)
 library(behaviouR)
 library(tuneR)
 library(seewave)
-library(ggplot2)
-library(dplyr)
 library(warbleR)
+library(dplyr)
+library(vegan)
 library(stringr)
-
-
-
-male_tawny_songs <- query_xc(qword = 'Strix aluco type:male type:song len:5-25', download = FALSE)
-
-female_tawny_songs <- query_xc(qword = 'Strix aluco type:female type:song len:5-25', download = FALSE)
+source("nes8010.R")
 
 
 
 
 
-map_xc(male_tawny_songs, leaflet.map = TRUE)
-map_xc(female_tawny_songs, leaflet.map = TRUE)
+tawny_male <- query_xc(qword = 'Strix aluco type:male len:5-25', download = FALSE)
+tawny_female <- query_xc(qword = 'Strix aluco type:female len:5-25', download = FALSE)
+
+map_xc(tawny_male, leaflet.map = TRUE)
+
+# # Create subfolders in your RStudio Project for song calls and alarm calls
+dir.create(file.path("tawny_male"))
+dir.create(file.path("tawny_female"))
+#
+# # Download the .MP3 files into two separate sub-folders
+query_xc(X = tawny_male, path="tawny_male")
+query_xc(X = tawny_female, path="tawny_female")
 
 
-# Create subfolders in your RStudio Project for song calls and alarm calls
-dir.create(file.path("male_tawny_songs"))
-dir.create(file.path("female_tawny_songs"))
 
-# Download the .MP3 files into two separate sub-folders
-query_xc(X = male_tawny_songs, path="male_tawny_songs")
-query_xc(X = female_tawny_songs, path="female_tawny_songs")
+#
+ old_files <- list.files("tawny_male", full.names=TRUE)
+ new_files <- NULL
+ for(file in 1:length(old_files)){
+   curr_file <- str_split(old_files[file], "-")
+   new_name <- str_c(c(curr_file[[1]][1:2], "-male_", curr_file[[1]][3]), collapse="")
+   new_files <- c(new_files, new_name)
+ }
+ file.rename(old_files, new_files)
 
+ old_files <- list.files("tawny_female", full.names=TRUE)
+ new_files <- NULL
+ for(file in 1:length(old_files)){
+   curr_file <- str_split(old_files[file], "-")
+   new_name <- str_c(c(curr_file[[1]][1:2], "-female_", curr_file[[1]][3]), collapse="")
+   new_files <- c(new_files, new_name)
+ }
+ file.rename(old_files, new_files)
 
-
-old_files <- list.files("male_tawny_songs", full.names=TRUE)
-new_files <- NULL
-for(file in 1:length(old_files)){
-  curr_file <- str_split(old_files[file], "-")
-  new_name <- str_c(c(curr_file[[1]][1:2], "-male_", curr_file[[1]][3]), collapse="")
-  new_files <- c(new_files, new_name)
-}
-file.rename(old_files, new_files)
-
-
-old_files <- list.files("female_tawny_songs", full.names=TRUE)
-new_files <- NULL
-for(file in 1:length(old_files)){
-  curr_file <- str_split(old_files[file], "-")
-  new_name <- str_c(c(curr_file[[1]][1:2], "-female_", curr_file[[1]][3]), collapse="")
-  new_files <- c(new_files, new_name)
-}
-file.rename(old_files, new_files)
-
-
+# Put blackbird files into appropriate folders ----
 dir.create(file.path("tawny_audio"))
-file.copy(from=paste0("male_tawny_songs/",list.files("male_tawny_songs")),
-          to="tawny_audio")
-file.copy(from=paste0("female_tawny_songs/",list.files("female_tawny_songs")),
-          to="tawny_audio")
+ file.copy(from=paste0("tawny_male/",list.files("tawny_male")),
+           to="tawny_audio")
+ file.copy(from=paste0("tawny_female/",list.files("tawny_female")),
+           to="tawny_audio")
 
-mp32wav(path="tawny_audio", dest.path="tawny_audio")
-unwanted_mp3 <- dir(path="tawny_audio", pattern="*.mp3")
-file.remove(paste0("tawny__audio/", unwanted_mp3))
-
-
-
-
-#Do I need to be showing an oscillogram and spectogram for the female too?
+# Convert to .WAV format and delete old mp3
+ mp32wav(path="tawny_audio", dest.path="tawny_audio")
+ unwanted_mp3 <- dir(path="tawny_audio", pattern="*.mp3")
+ file.remove(paste0("tawny_audio/", unwanted_mp3))
 
 
-male_tawny_wav <- readWave("tawny_audio/Strixaluco-male_506715.wav")
-male_tawny_wav
 
-oscillo(male_tawny_wav)
 
-oscillo(male_tawny_wav, from = 0.59, to = 0.60)
+tawny_male_wav <- readWave("tawny_audio/Strixaluco-male_506715.wav")
+tawny_male_wav
+
+oscillo(tawny_male_wav)
+
+#Change this at a later point to make sure it lines up with female
+oscillo(tawny_male_wav, from = 0.59, to = 0.60)
 
 
 SpectrogramSingle(sound.file = "tawny_audio/Strixaluco-male_506715.wav",
@@ -82,12 +79,12 @@ SpectrogramSingle(sound.file = "tawny_audio/Strixaluco-male_506715.wav",
 
 
 
-female_tawny_wav <- readWave("tawny_audio/Strixaluco-female_343923.wav")
-female_tawny_wav
+tawny_female_wav <- readWave("tawny_audio2/Strixaluco-female_343923.wav")
+tawny_female_wav
 
-oscillo(female_tawny_wav)
+oscillo(tawny_female_wav)
 
-oscillo(female_tawny_wav, from = 0.59, to = 0.60)
+oscillo(tawny_female_wav, from = 0.59, to = 0.60)
 
 
 SpectrogramSingle(sound.file = "tawny_audio/Strixaluco-female_343923.wav",
@@ -96,22 +93,18 @@ SpectrogramSingle(sound.file = "tawny_audio/Strixaluco-female_343923.wav",
 
 
 
+# Feature extraction for blackbirds via MFCC and PCA ####
+tawny_mfcc <- MFCCFunction(input.dir = "tawny_audio",
+                               max.freq=7000)
+dim(tawny_mfcc)
+tawny_pca <- ordi_pca(tawny_mfcc[, -1], scale=TRUE)
+summary(tawny_pca)$cont[[1]][1:3,1:4]
 
+# Plot PCA scores
+tawny_sco <- ordi_scores(tawny_pca, display="sites")
+tawny_sco <- mutate(tawny_sco, group_code = tawny_mfcc$Class)
 
+ggplot(tawny_sco, aes(x=PC1, y=PC2, colour=group_code)) +
+  geom_point() 
 
-
-
-
-
-
-
-
-songthrush_songs <- query_xc(qword = 'Turdus philomelos  type:song len:5-25', download = FALSE)
-
-songthrush_alarm <- query_xc(qword = 'Turdus philomelos  type:alarm len:5-25', download = FALSE)
-
-
-blackcap_songs <- query_xc(qword = 'Sylvia atricapilla  type:song len:5-25', download = FALSE)
-
-blackcap_alarm <- query_xc(qword = 'Sylvia atricapilla  type:alarm len:5-25', download = FALSE)
 
